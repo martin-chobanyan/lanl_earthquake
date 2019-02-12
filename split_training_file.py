865d6f1abe06e2d34eb2c7f5dc610bcc29d71d50
Martin Chobanyan <chobanyan.martin@gmail.com>
2019-02-12 00:30:06 -0500

    fixed split_training_file so that it works (tab issues)

--- split_training_file.py
+++ split_training_file.py
@@ -7,8 +7,8 @@ import os
 import csv
 
 
-# an arbitrary value signifying a new earthquake segment
-THRESHOLD = 0.01
+THRESHOLD = 0.01  # an arbitrary value signifying a new earthquake segment
+root_folder = '/home/mchobanyan/data/kaggle/lanl_earthquake/'
 
 
 def main():
@@ -17,37 +17,37 @@ def main():
 	prev_time = None
 	header = None
 	segment = []
-	with open(train_file, 'r') as main_file:
+	with open(os.path.join(root_folder, 'train.csv'), 'r') as main_file:
 	    
-	    i = 0
-	    file_reader = csv.reader(main_file)
-	    for line in file_reader:
+		i = 0
+		file_reader = csv.reader(main_file)
+		for line in file_reader:
 	        
-	        if i == 0:
-	            header = line
-	        else:
-	            
-	            time_left = float(line[1])
+			if i == 0:
+				header = line
+			else:
+			    
+				time_left = float(line[1])
 
-	            if i == 1:
-	                prev_time = time_left
-	                
-	            elif time_left-prev_time > THRESHOLD:
+				if i == 1:
+				    prev_time = time_left
+				    
+				elif time_left-prev_time > THRESHOLD:
 
-	                # write the segment to a new file
-	                filename = f'segment{segment_id}.csv'
-	                with open(os.path.join(root_folder, 'train', filename), 'w') as segment_file:
-	                    file_writer = csv.writer(segment_file)
-	                    for line_out in segment:
-	                        file_writer.writerow(line_out)
-	                    segment_id += 1
-	                    segment = [header]
-	                    print(f'Created {filename} after {i} lines...')
-	                    
-	            prev_time = time_left
+				    # write the segment to a new file
+				    filename = f'segment{segment_id}.csv'
+				    with open(os.path.join(root_folder, 'train', filename), 'w') as segment_file:
+				        file_writer = csv.writer(segment_file)
+				        for line_out in segment:
+				            file_writer.writerow(line_out)
+				        segment_id += 1
+				        segment = [header]
+				        print(f'Created {filename} after {i} lines...')
+				        
+				prev_time = time_left
 
-            segment.append(line)
-            i += 1
+			segment.append(line)
+			i += 1
 
 
 if __name__ == '__main__':
